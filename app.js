8f4ca291cbb6d28dab60d086f386b30b78fe130f
Victor Hugo Meurer <meurervictorhugo@gmail.com>
2022-06-26 18:06:25 -0300

    Feat: add POST/participants

---
+++ app.js
@@ -0,0 +1,54 @@
+import express, {json} from 'express';
+import cors from 'cors';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+import dayjs from 'dayjs';
+import joi from 'joi';
+dotenv.config();
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+const participantSchema = joi.object({
+    name: joi.string().required(),
+});
+
+app.post('/participants', async (req, res) => {
+    const participant = req.body;
+    const validation = participantSchema.validate(participant);
+
+    if (validation.error) {
+        return res.sendStatus(422);
+    }
+
+    try {
+        const mongoClient = new MongoClient(process.env.MONGO_URI)
+    await mongoClient.connect()
+
+    const participantsCollection = mongoClient.db('bate-papo-uol').collection('participants');
+    const messagesCollection = mongoClient.db('bate-papo-uol').collection('messages');
+
+    const registeredParticipant = await participantsCollection.findOne({name: participant.name});
+    if (registeredParticipant){
+        return res.sendStatus(409);
+    }
+
+    await participantsCollection.insertOne({name: participant.name, lastStatus: Date.now()});
+    await messagesCollection.insertOne({
+        from: participant.name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: dayjs().format('HH:mm:ss')
+    });
+    mongoClient.close();
+    res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
